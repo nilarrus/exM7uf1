aa117201033abb0a3f758c6d765ae3c0748371a1
nilarrus <nilarrus69@gmail.com>
2018-09-21 17:32:00 +0200

    Fet fins el ex 7

--- ex1/Ej5.php
+++ ex1/Ej5.php
@@ -1,4 +1,4 @@
 <?php
 echo "5.Digues per a que serveix la funció gettype.";
-echo "Sirbe para ";
+echo "Sirbe para mostar el tipo de valor de la variable en la funcion";
  ?>
--- ex1/Ej7.php
+++ ex1/Ej7.php
@@ -1,3 +1,7 @@
 <?php
 echo "7.Modifica el codi per saber de quin tipus és el valor que retorna gettype.";
+$i = 1;
+$tipus_de_i = gettype( $i );
+$ttdei = gettype($tipus_de_i);
+  echo "La variable \$tipus_de_i conté el valor $tipus_de_i i és del tipus $ttdei <br>";
  ?>
